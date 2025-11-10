b908ee3a8e3530036f059c260f4e7f4beaeedd77
Matthew S Cherney <matt66146@gmail.com>
2025-11-10 09:12:33 -0800

    daily2169

---
+++ Daily2169/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Daily2169;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+public class Solution
+{
+    public int CountOperations(int num1, int num2)
+    {
+        int count = 0;
+        while (num1 > 0 && num2 > 0)
+        {
+            if (num1 > num2)
+            {
+                num1 -= num2;
+            }
+            else
+            {
+                num2 -= num1;
+            }
+            count++;
+        }
+        return count;
+    }
+}
