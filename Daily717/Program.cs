9249d4eb25a355154fa1db20ba4f0ef1cbc9c01c
Matthew S Cherney <matt66146@gmail.com>
2025-11-17 18:59:37 -0800

    v2 much faster on avg vs v1 for extremely large arrays

---
+++ Daily717/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Daily717;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var solution = new Solution();
+
+        Console.WriteLine(solution.IsOneBitCharacter([1, 0, 0]));
+        Console.WriteLine(solution.IsOneBitCharacter([1, 1, 1, 0]));
+
+    }
+}
+public class Solution
+{
+    public bool IsOneBitCharacter(int[] bits)
+    {
+
+
+        for (int i = 0; i < bits.Length; i++)
+        {
+
+            if (bits[i] == 1)
+            {
+                i++;
+            }
+            else
+            {
+                if (i == bits.Length - 1) return true;
+            }
+        }
+        return false;
+
+    }
+    public bool IsOneBitCharacterV2(int[] bits)
+    {
+        int ones = 0;
+
+        for (int i = bits.Length - 2; i >= 0 && bits[i] != 0; i--)
+        {
+            ones++;
+        }
+        if (ones % 2 != 0) return false;
+        return true;
+    }
+}
