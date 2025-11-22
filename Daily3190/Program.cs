1a0393a484215788ed1b4d76bbdd414f20e328bb
Matthew S Cherney <matt66146@gmail.com>
2025-11-21 16:28:49 -0800

    ez

---
+++ Daily3190/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Daily3190;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        Console.WriteLine(solution.MinimumOperations([1, 2, 3, 4]));
+        Console.WriteLine(solution.MinimumOperations([3, 6, 9]));
+    }
+}
+public class Solution
+{
+    public int MinimumOperations(int[] nums)
+    {
+        int numOperations = 0;
+
+        foreach (int num in nums)
+        {
+            if (num % 3 != 0)
+            {
+                numOperations++;
+            }
+        }
+
+        return numOperations;
+    }
+}
