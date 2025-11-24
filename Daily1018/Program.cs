2311d4d46c47d771d05c504c136ea079ca824562
Matthew S Cherney <matt66146@hotmail.com>
2025-11-23 21:25:36 -0800

    done

---
+++ Daily1018/Program.cs
@@ -0,0 +1,30 @@
+﻿
+Solution solution = new();
+
+
+
+
+
+Console.WriteLine(string.Join(",", solution.PrefixesDivBy5([1, 1, 0, 0, 0, 1, 0, 0, 1])));
+Console.WriteLine(string.Join(",", solution.PrefixesDivBy5([0, 1, 1])));
+Console.WriteLine(string.Join(",", solution.PrefixesDivBy5([1, 1, 1])));
+//Console.WriteLine(string.Join(",", solution.PrefixesDivBy5([1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0])));
+//Console.WriteLine(string.Join(",", solution.PrefixesDivBy5([1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0])));
+
+
+public class Solution
+{
+    public IList<bool> PrefixesDivBy5(int[] nums)
+    {
+       var result = new bool[nums.Length];
+        int value = 0;
+        for(int i  = 0; i < nums.Length;i++)
+        {
+            value = (value << 1 + nums[i]) % 5;
+
+            result[i] = (value == 0);
+        }
+
+        return result;
+    }
+}
