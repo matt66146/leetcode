bd0fa963a4611487e22ff4bf946efa28470c1452
Matthew S Cherney <matt66146@gmail.com>
2025-11-28 17:13:36 -0800

    done

---
+++ Daily3512/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Daily3512;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Solution sol = new Solution();
+        Console.WriteLine(sol.MinOperations2([3, 9, 7], 5));
+    }
+}
+
+public class Solution
+{
+    public int MinOperations(int[] nums, int k)
+    {
+        int sum = 0;
+        foreach (var num in nums)
+            sum += num;
+        return sum % k;
+    }
+    public int MinOperations2(int[] nums, int k)
+    {
+        int sum = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            sum += nums[i];
+        }
+        return sum - (sum / k) * k;
+    }
+
+}
