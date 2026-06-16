ffc1643255c962733c3b26870d0f296039151ef4
Matthew S Cherney <matt66146@gmail.com>
2026-06-16 10:42:36 -0700

    w/e good enough.

---
+++ Daily3612/Program.cs
@@ -0,0 +1,95 @@
+﻿Solution solution = new();
+
+
+String result = solution.ProcessStr("a#b%*");
+
+Console.WriteLine(result);
+
+
+
+
+
+public class Solution
+{
+
+
+    public string ProcessStr(string s)
+    {
+        string result = "";
+        bool reverse = false;
+        Console.WriteLine(result.Length);
+        foreach (var c in s)
+        {
+            switch (c)
+            {
+                case '*':
+                    if (result.Length > 0)
+                    {
+                        if (reverse)
+                        {
+                            result = result[1..];
+                        }
+                        else
+                        {
+                            result = result.Remove(result.Length - 1);
+                        }
+
+                    }
+                    break;
+                case '#':
+                    result += result;
+                    break;
+                case '%':
+                    reverse = !reverse;
+                    //result = new string(result.Reverse().ToArray());
+                    break;
+                default:
+                    if (reverse)
+                    {
+                        result = c + result;
+                    }
+                    else
+                    {
+                        result += c;
+                    }
+
+                    break;
+
+            }
+        }
+
+        if (reverse) result = new string(result.Reverse().ToArray());
+        return result;
+    }
+
+    public string ProcessStrV1(string s)
+    {
+        string result = "";
+        Console.WriteLine(result.Length);
+        foreach (var c in s)
+        {
+            switch (c)
+            {
+                case '*':
+                    if (result.Length > 0)
+                    {
+                        result = result.Remove(result.Length - 1);
+                    }
+                    break;
+                case '#':
+                    result += result;
+                    break;
+                case '%':
+                    result = new string(result.Reverse().ToArray());
+                    break;
+                default:
+                    result += c;
+                    break;
+
+            }
+        }
+
+
+        return result;
+    }
+}
