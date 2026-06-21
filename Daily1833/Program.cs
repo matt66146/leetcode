bb8b643266b8c8043466d0dd15ed6ad8c4ee354d
Matthew S Cherney <matt66146@gmail.com>
2026-06-21 14:37:43 -0700

    daily 1833

---
+++ Daily1833/Program.cs
@@ -0,0 +1,27 @@
+﻿Solution solution = new();
+
+Console.WriteLine(solution.MaxIceCream([10, 6, 8, 7, 7, 8], 5));
+
+
+public class Solution
+{
+    public int MaxIceCream(int[] costs, int coins)
+    {
+        costs.Sort();
+        int total = 0;
+        foreach (var icecream in costs)
+        {
+            if (icecream <= coins)
+            {
+                coins -= icecream;
+                total++;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return total;
+    }
+}
