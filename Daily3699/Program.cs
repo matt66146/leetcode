2a4a0b62f7e51960b206b3b3e8c6348b9a2dbb27
Matthew Cherney <matt66146@gmail.com>
2026-06-23 04:12:38 -0700

    daily 3699 start

---
+++ Daily3699/Program.cs
@@ -0,0 +1,15 @@
+﻿Solution solution = new();
+
+Console.WriteLine(solution.ZigZagArrays(3, 4, 5));
+
+public class Solution
+{
+
+    public int ZigZagArrays(int n, int l, int r)
+    {
+        int answer = 0;
+
+
+        return answer;
+    }
+}
